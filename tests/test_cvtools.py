e56b82a85f18e1f81cc816a0b4a4aadd74b5dce2
Fafa <fafafft@gmail.com>
2018-02-21 09:41:49 +0100

    unit tests and regression updated

--- tests/test_cvtools.py
+++ tests/test_cvtools.py
@@ -34,15 +34,42 @@ class TestFeatures(unittest.TestCase):
         self.assertAlmostEqual(2.781, features['minor_axis_length'], 2)
         self.assertAlmostEqual(0.068, features['orientation'], 2)
         self.assertEqual(0.01, features['clipped_fraction'])
+        self.assertEqual(0.875, features['solidity'])  # is it spread a bit
 
     def test_intensity_object(self):
         red_blue_blob = self.red_blob.copy()
-        red_blue_blob[5:8, 2:7, 1] = 20
+        red_blue_blob[5:8, 2:7, 2] = 20
         features = quick_features(red_blue_blob)
-        # test only that they are calculated for all colours
-        # and added to results
 
-        self.fail("TODO - test calculated values")
+        red_intensity = features['intensity_red']
+        self.assertAlmostEqual(129, red_intensity['median_intensity'], 1)
+        self.assertAlmostEqual(79, red_intensity['mean_intensity'], 0)  # it includes some borders
+        self.assertGreater(70, red_intensity['std_intensity'], 3)
+        self.assertAlmostEqual(1, red_intensity['perc_25_intensity'], 3)
+        self.assertAlmostEqual(129, red_intensity['perc_75_intensity'], 3)
+
+        blue_intensity = features['intensity_blue']
+        self.assertAlmostEqual(20, blue_intensity['median_intensity'], 1)
+        self.assertAlmostEqual(15.7, blue_intensity['mean_intensity'], 0)  # it includes some borders
+        self.assertGreater(8, blue_intensity['std_intensity'], 3)
+        self.assertAlmostEqual(20, blue_intensity['perc_25_intensity'], 3)
+        self.assertAlmostEqual(20, blue_intensity['perc_75_intensity'], 3)
+
+        gray_intensity = features['intensity_gray']
+        self.assertAlmostEqual(49, gray_intensity['median_intensity'], 1)
+        self.assertAlmostEqual(31.5, gray_intensity['mean_intensity'], 0)
+        self.assertAlmostEqual(22.85, gray_intensity['std_intensity'], 1)
+        self.assertAlmostEqual(6, gray_intensity['perc_25_intensity'], 3)
+        self.assertAlmostEqual(50, gray_intensity['perc_75_intensity'], 3)
+
+        # there is nothing in green only noise
+        green_intensity = features['intensity_green']
+        self.assertGreater(1, green_intensity['mean_intensity'])
+        self.assertGreater(1, green_intensity['std_intensity'], 3)
+        self.assertAlmostEqual(0, green_intensity['perc_25_intensity'], 3)
+        self.assertAlmostEqual(1, green_intensity['perc_75_intensity'], 3)
+        self.assertGreater(0.1, green_intensity['mass_displace_in_images'], 3)
+        self.assertGreater(0.1, green_intensity['mass_displace_in_minors'], 3)
 
     def test_intensity_measures_rect(self):
         # std, quartile intensity
@@ -67,10 +94,19 @@ class TestFeatures(unittest.TestCase):
         self.assertAlmostEqual(expected_displacement_image, features['mass_displace_in_images'], 3)
         self.assertAlmostEqual(expected_displacement_relative, features['mass_displace_in_minors'], 3)
 
-    def test_intensity_measures_circle(self):
-        # test with circle
-        # circle will have some gradient
-        self.fail("TODO")
+    def test_intensity_measures_line(self):
+        line = np.zeros((20, 20), dtype=np.uint8)
+        line[5, 8:18] = np.arange(1, 11)
+        line[6, 8:18] = np.arange(1, 11)
+
+        features = intensity_features(line, line > 0)
+        self.assertAlmostEqual(5.5, features['median_intensity'])
+        self.assertAlmostEqual(5.5, features['mean_intensity'])
+        self.assertAlmostEqual(2.87228, features['std_intensity'], 3)
+        self.assertAlmostEqual(3, features['perc_25_intensity'], 3)
+        self.assertAlmostEqual(8, features['perc_75_intensity'], 3)
+        self.assertAlmostEqual(0.075, features['mass_displace_in_images'], 3)
+        self.assertAlmostEqual(0.75, features['mass_displace_in_minors'], 3)
 
     def test_raw_object_regression(self):
         raw_8bit = convert_to_8bit(self.raw_image)
@@ -78,15 +114,8 @@ class TestFeatures(unittest.TestCase):
         with open("expected/case_1_blob_qf.pickle", "rb") as f:
             expected_features = pickle.load(f)
 
-        # pickle.dump(features, open("expected/case_1_blob_qf.pickle", "wb"))
+        #pickle.dump(features, open("expected/case_1_blob_qf.pickle", "wb"))
         expect_rep = sorted(expected_features)
-
-        # ignore added features
-        del features['solidity']
-        del features['estimated_volume']
-        del features['eccentricity']
-        for k in [k for k in features.keys() if 'intensity' in k]:
-            del features[k]
 
         features_rep = sorted(features)
         self.assertEqual(str(expect_rep), str(features_rep))
