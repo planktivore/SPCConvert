86ee5a5d6b5a785c3157b754d63d5b8f6a64df4a
igneel73 <igneel.kush73@gmail.com>
2019-06-01 22:34:40 -0700

    annotation tool complete

--- app/server.py
+++ app/server.py
@@ -17,12 +17,31 @@ def output():
 
 @app.route("/save_gtruth", methods = ['POST'])
 def save():
-    data = request.get_json(force=True)
-    result = ''
 
-    # take ground truth values
+	data = request.get_json(force=True)
+	result = ''
 
-    return result
+	# load in predictions.json
+	prediction = pd.read_json("predictions.json")
+
+	# add gtruth values to predictions
+	url_to_label = {}
+	for image in data:
+		name = image["url"]
+		name = name[:(len(name) - 5)]
+		name = name[13:]
+		name = name + ".tif"
+		gtruth = image["gtruth"]
+		url_to_label[name] = gtruth
+
+	for i in range(len(prediction['machine_labels'])):
+		url = prediction['machine_labels'].iloc[i]['image_id']
+		prediction['machine_labels'].iloc[i]['gtruth'] = url_to_label[url]
+
+	# write back to predictions.json
+	prediction.to_json("predictions.json")
+
+	return result
 
 if __name__ == "__main__":
     app.run()
--- sampleImgs/sampleImgs_static_html/server.py
+++ sampleImgs/sampleImgs_static_html/server.py
@@ -3,6 +3,8 @@ import sys
 from flask import Flask, render_template, request, redirect, Response
 import random, json
 
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,12 +17,31 @@ def output():
 
 @app.route("/save_gtruth", methods = ['POST'])
 def save():
-    data = request.get_json(force=True)
-    result = ''
 
-    print(data)
+	data = request.get_json(force=True)
+	result = ''
 
-    return result
+	# load in predictions.json
+	prediction = pd.read_json("predictions.json")
+
+	# add gtruth values to predictions
+	url_to_label = {}
+	for image in data:
+		name = image["url"]
+		name = name[:(len(name) - 5)]
+		name = name[13:]
+		name = name + ".tif"
+		gtruth = image["gtruth"]
+		url_to_label[name] = gtruth
+
+	for i in range(len(prediction['machine_labels'])):
+		url = prediction['machine_labels'].iloc[i]['image_id']
+		prediction['machine_labels'].iloc[i]['gtruth'] = url_to_label[url]
+
+	# write back to predictions.json
+	prediction.to_json("predictions.json")
+
+	return result
 
 if __name__ == "__main__":
     app.run()
