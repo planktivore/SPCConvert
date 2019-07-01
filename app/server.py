ab8e6048c9aa6a99d4f3775b19bc2c1c70b875e9
igneel73 <igneel.kush73@gmail.com>
2019-07-01 11:47:53 -0700

    fixed saving

--- app/server.py
+++ app/server.py
@@ -7,6 +7,38 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# helper functions
+def loadDB(db_path):
+    """Loads db into list of entries
+
+    Args:
+        db_fname (str): Abs path to the db
+
+    Returns:
+        list: List of db values
+
+    """
+    curr_db = ""
+    with open(db_path, "r") as fconv:
+        curr_db = fconv.read()
+
+    db_entries = to_json_format(curr_db)
+    return db_entries
+
+
+def to_json_format(str_db):
+    """Convert str db into python list of db values (json format)"""
+    import json
+    ind_opbrac = str_db.find("(") + 1
+    str_db = str_db[ind_opbrac:]
+
+    ind_brac = str_db.find(")")
+
+    str_db = str_db[:ind_brac]
+    ind_last_comma = len(str_db) - 2
+    str_db = str_db[:ind_last_comma - 1] + str_db[ind_last_comma:]
+    return json.loads(str_db)
+
 @app.route("/")
 def home():
     return render_template('spcdata.html')
@@ -30,14 +62,26 @@ def save():
 		gtruth = image["gtruth"]
 		url_to_label[name] = gtruth
 
-	for i in range(len(prediction['machine_labels'])):
-		url = prediction['machine_labels'].iloc[i]['image_id']
-		prediction['machine_labels'].iloc[i]['gtruth'] = url_to_label[url]
+	# save the new predictions to the roistore
+	# load in database file
+	entries = loadDB("./static/js/database.js")
 
-	# write back to predictions.json
-	prediction.to_json("predictions.json")
+	# update values in the list
+	for entry in entries:
+		filename = entry['url'][13:]
+		filename = filename[:len(filename) - 5]
+		filename = filename + ".tif"
+		entry["gtruth"] = url_to_label[filename]
+
+	# write a new db from the db template with the updated list
+	db_str = json.dumps(entries)
+	db_str = "roistore = TAFFY(" + db_str + ");"
+
+	with open("./static/js/database.js", "w") as fconv:
+		fconv.write(db_str)
 
 	return result
 
 if __name__ == "__main__":
+
     app.run()
